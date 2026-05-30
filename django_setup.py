ebe02d8b8bbfb59d28194d92bf3453dc52860330
Nordenchik <nordenchik228@gmail.com>
2026-05-30 14:27:40 +0300

    v1.0.0

--- django_setup.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python_advance_m3l4.settings")
-django.setup()
--- main.py
+++
@@ -1,6 +0,0 @@
-import django_setup
-from lesson import models
-from lesson.models import *
-
-some_user = models.User(username='user_1', email='email@gmail.com', role='admin')
-some_user.save()
